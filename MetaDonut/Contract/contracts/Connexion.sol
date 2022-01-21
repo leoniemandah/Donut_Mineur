abd64671268ca73217f1c68289e1416d18bc55a0
idris meznad <mezdidis@hotmail.fr>
2022-01-21 12:26:35 +0100

    clickfunction

--- MetaDonut/Contract/contracts/Connexion.sol
+++ MetaDonut/Contract/contracts/Connexion.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor() ERC20("Donut", "DNT") {
     _mint(msg.sender, 100000000000000000000000000000000);
     }
+    function click() public{
+        _mint(msg.sender, 1);
+    }
 }
+
