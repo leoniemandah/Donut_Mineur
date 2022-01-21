bfa81861b0ee3662e8a302c101a9ece9b0158bd4
idris meznad <mezdidis@hotmail.fr>
2022-01-21 15:17:22 +0100

    add

--- web/Web3.js
+++ web/Web3.js
@@ -2,13 +2,13 @@ addressContrat = "0x134A2064BF8311B58a7e14bB0EDfd6B4ef9859B2";
 const webconnect = new Web3(window.ethereum);
 const MyToken = new webconnect.eth.Contract(Tokenabi.abi, addressContrat);
 //await MyToken.methods.Click().Call();
-const getData = Async() => {
+const getData = async() => {
     const signer = await webconnect.getSigner();
     console.log(signer);
 }
 await MyToken.methods.balanceOf(signer).call();
 
-const getAddr = Async() => {
+const getAddr = async() => {
     const useraddr = await webconnect.getAddress();
     console.log(useraddr);
 }
