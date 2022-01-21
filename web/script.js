de37223fec0dd177ae361740dac890ef923838f0
Dercraker <antoine.capitain@gmail.com>
2022-01-21 14:31:28 +0100

    Cookie ready 1 Error
    error on number of donnut

--- web/script.js
+++ web/script.js
@@ -3,16 +3,22 @@ var btnBonus1;
 var btnBonus2;
 var btnBonus3;
 var compteurDnt;
-var listeBonus;
 var BonusInfo1;
 var BonusInfo2;
 var BonusInfo3;
-var addDonut1;
-var addDonut2;
-var addDonut3;
+var addDonut1 = 0;
+var addDonut2 = 0;
+var addDonut3 = 0;
+var listeBonus = [0, 0, 0]
+var EnumBonus = {
+    Bonus1: 0,
+    Bonus2: 1,
+    Bonus3: 2
+}
+var CookieBonus;
 
 function initGame() {
-    nbDonuts = 1000;
+    nbDonuts = 0;
     btnBonus1 = document.getElementById('Buy1');
     btnBonus2 = document.getElementById('Buy2');
     btnBonus3 = document.getElementById('Buy3');
@@ -21,16 +27,20 @@ function initGame() {
     BonusInfo3 = document.querySelector('.bonusInfo3 p');
     compteurDnt = document.getElementById('profit');
     profitDNT = document.getElementById('profitDNT');
+
+
+    initCookiesValues();
+    // nbDonuts = 0;
     compteurDnt.innerText = nbDonuts + ' Donuts';
     profitDNT.innerText = nbDonuts / 1000 + ' DNT';
-    profitDNT.innerText = nbDonuts / 1000 + ' DNT';
+
     unlockBonus1();
     unlockBonus2();
     unlockBonus3();
 }
 
 function clickDonut() {
-    nbDonuts = nbDonuts + 1;
+    nbDonuts = nbDonuts + 1000;
     compteurDnt.innerText = nbDonuts + ' Donuts';
     profitDNT.innerText = nbDonuts / 1000 + ' DNT';
     unlockBonus1();
@@ -121,7 +131,7 @@ function bonus3() {
 
 function buyBonus1() {
     //listeBonus.push('bonus1');
-    if (addDonut1 == null) {
+    if (addDonut1 == 0) {
         addDonut1 = 1
         BonusInfo1.innerText = `Permets de générer ${addDonut1} Donuts toutes les 5 secondes`
         bonus1()
@@ -129,6 +139,10 @@ function buyBonus1() {
         addDonut1++
         BonusInfo1.innerText = `Permets de générer ${addDonut1} Donuts toutes les 5 secondes`
     }
+
+    listeBonus[EnumBonus.Bonus1] = addDonut1
+    setCookie("listeBonus", listeBonus, 1);
+
     nbDonuts = nbDonuts - 15;
     compteurDnt.innerText = nbDonuts + ' Donuts';
     profitDNT.innerText = nbDonuts / 1000 + ' DNT';
@@ -139,7 +153,7 @@ function buyBonus1() {
 
 function buyBonus2() {
     //listeBonus.push('bonus2');
-    if (addDonut2 == null) {
+    if (addDonut2 == 0) {
         addDonut2 = 50
         BonusInfo2.innerText = `Permets de générer ${addDonut2} Donuts toutes les 5 secondes`
         bonus2()
@@ -147,6 +161,10 @@ function buyBonus2() {
         addDonut2 += 50
         BonusInfo2.innerText = `Permets de générer ${addDonut2} Donuts toutes les 5 secondes`
     }
+
+    listeBonus[EnumBonus.Bonus2] = addDonut2
+    setCookie("listeBonus", listeBonus, 1);
+
     nbDonuts = nbDonuts - 150;
     compteurDnt.innerText = nbDonuts + ' Donuts';
     profitDNT.innerText = nbDonuts / 1000 + ' DNT';
@@ -156,8 +174,7 @@ function buyBonus2() {
 }
 
 function buyBonus3() {
-    //listeBonus.push('bonus3');
-    if (addDonut3 == null) {
+    if (addDonut3 == 0) {
         addDonut3 = 100
         BonusInfo3.innerText = `Permets de générer ${addDonut3} Donuts toutes les 15 secondes`
         bonus3()
@@ -165,6 +182,8 @@ function buyBonus3() {
         addDonut3 += 100
         BonusInfo3.innerText = `Permets de générer ${addDonut3} Donuts toutes les 15 secondes`
     }
+    listeBonus[EnumBonus.Bonus3] = addDonut3
+    setCookie("listeBonus", listeBonus, 1);
 
     nbDonuts = nbDonuts - 1000;
     compteurDnt.innerText = nbDonuts + ' Donuts';
@@ -172,4 +191,49 @@ function buyBonus3() {
     lockBonus1();
     lockBonus2();
     lockBonus3();
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+
+    var cookieArr = document.cookie.split(";");
+
+    for (var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+
+        if (cname == cookiePair[0]) {
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+
+    return null;
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        alert("Ready to mine " + user + " ?");
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+
+function initCookiesValues() {
+    CookieBonus = getCookie("listeBonus");
+    if (!CookieBonus) return null;
+    addDonut1 = Number(CookieBonus.split(',')[EnumBonus.Bonus1]);
+    bonus1();
+    addDonut2 = Number(CookieBonus.split(',')[EnumBonus.Bonus2]);
+    bonus2();
+    addDonut3 = Number(CookieBonus.split(',')[EnumBonus.Bonus3]);
+    bonus3();
 }
